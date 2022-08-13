ecd3c85ffe9ba8d7dcb4a34e7183c01156487d4b
datmax <max.datsun.07@gmail.com>
2022-08-13 16:51:18 +0200

    updated index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ethers = require("ethers");
 //SEEEEH
 const privateKey = "insert key"; 
 const msg = "ciao";
-const signer = new ethers.Wallet("")
+const signer = new ethers.Wallet(privateKey)
 signer.signMessage("ciao").then((msg) =>{
     console.log(msg)
 })
